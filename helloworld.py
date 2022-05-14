1f7572242e4a37d0004028d33c00c18fff7f5fc6
studenteconomist <ookrylova2018@gmail.com>
2022-05-14 16:53:45 +0300

    плотли импортирован, альтаир работает

--- helloworld.py
+++ helloworld.py
@@ -1,81 +1,52 @@
+import plotly as plotly
 import streamlit as st
 import pandas as pd
 import numpy as np
 import seaborn as sns
-import matplotlib.pyplot as plt
 import altair as alt
+import matplotlib.pyplot as plt
 
 with st.echo(code_location='below'):
     """
-    ## Hello, World!
+    ## мой dataset movies
     """
-
-
-    def print_hello(name="World"):
-        st.write(f"### Hello, {name}!")
-
-
-    name = st.text_input("Your name", key="name", value="Anonymous")
-    print_hello(name)
-
-    """
-    ## Добавим графики
-    Чтобы заработали библиотеки seaborn и altair, нужно добавить в проект файл 
-    `requirements.txt` с такими строчками:
-    
-        seaborn
-        altair
-    """
-
-
-    a = st.slider("a")
-    x = np.linspace(-6, 6, 500)
-    df = pd.DataFrame(dict(x=x, y=np.sin(a * x)))
-    fig, ax = plt.subplots()
-    sns.lineplot(data=df, x="x", y="y", ax=ax)
-    st.pyplot(fig)
-
-    """
-    ## Немного анализа данных
-    """
-
 
     @st.cache
     def get_data():
         data_url = (
-            "https://github.com/Godoy/imdb-5000-movie-dataset/raw/"
-            "master/data/movie_metadata.csv"
+        "https://raw.githubusercontent.com/ookrylova/reposetory-1/"
+        "master/movies.csv"
         )
         return (
-            pd.read_csv(data_url)
-            .dropna(subset=["title_year"])
-            .assign(
-                title_year=lambda x: pd.to_datetime(
-                    x["title_year"], format="%Y"
-                )
-            )
-        )
-
+        pd.read_csv(data_url))
 
     df = get_data()
+    new_order =df.groupby(by=["genre"])["runtime"].median().iloc[::-1].index
+    """
+    # fig.1 зависимость длинны фильма отжанра
+    """
+    fig, ax = plt.subplots()
+    sns.violinplot(x='genre', y='runtime', data=df, order=new_order)
+    plt.xticks(rotation=90)
+    plt.title("длины фильмов разных жанров")
+    st.pyplot(fig)
 
-    director = st.selectbox(
-        "Director", df["director_name"].value_counts().iloc[:10].index
-    )
+    """
+    ## столбики показывают связь бюджета и разрешенности детям
+    """
+    fig, ax = plt.subplots()
+    sns.barplot(x="budget", y="mpaa_rating", data=df)
+    st.pyplot(fig)
 
-    df_selection = df[lambda x: x["director_name"] == director]
-    df_selection
+    """
+        ## альтаир
+    """
 
-    chart = (
-        alt.Chart(df_selection)
-        .mark_circle()
-        .encode(x=alt.X("title_year:T"), y="imdb_score", tooltip="movie_title")
-    )
+    fig1 = alt.Chart(df).mark_point().encode(x='budget', y='runtime', color='genre',
+                                        tooltip=['release_date', 'budget']).interactive()
 
     st.altair_chart(
         (
-            chart
-            + chart.transform_loess("title_year", "imdb_score").mark_line()
+            fig1
         ).interactive()
-        # .transform_loess добавляет сглаживающую кривую
     )
