fbb9c6c5df6547b2a4baf5890e1f4154c76424b7
studenteconomist <105130268+studenteconomist@users.noreply.github.com>
2022-06-13 22:49:34 +0300

    летний проект

--- helloworld.py
+++ helloworld.py
@@ -8,48 +8,38 @@ import matplotlib.pyplot as plt
 
 with st.echo(code_location='below'):
     """
-    # датасет movies (с ресурса data.world, загружен пользователем jamesgaskin)
+    # поработаю с тем же датасетом, который использовался в питоновском файле в другой части моего проекта. датасет про количество смерти ей заболевших по counties (областям внутри штатов США). в юпитере я избавилась от столбца counties, но сейчас нарисую про него график. 
+    оставлю из этой огромной таблицы данные только за 11 июня 2022, но даже после этого в таблице 3 тысяч строк, так что на стримлете запустится не сразу. столбец FIPS – это номер county, т.е. области внутри штата.  
     """
 
     @st.cache
     def get_data():
         data_url = (
-        "https://raw.githubusercontent.com/studenteconomist/reposetory-1/"
-        "master/movies.csv"
+        "https://raw.githubusercontent.com/nytimes/covid-19-data/"
+        "master/us-counties-recent.csv"
         )
         return (
         pd.read_csv(data_url))
 
-    df = get_data()
-    new_order =df.groupby(by=["genre"])["runtime"].median().iloc[::-1].index
+    df_covid = get_data()
+    df_cov_11 = df_covid[df_covid['date'] == '2022-06-11']
     """
     ##  библиотека seaborn. 
-    ### рис. 1 зависимость длинны фильма отжанра
+    ### рис. 1 зависимость кол-ва кейсов от county
     """
     fig, ax = plt.subplots()
-    sns.violinplot(x='genre', y='runtime', data=df, order=new_order)
+    sns.violinplot(x='county', y='cases', data=df_cov_11)
     plt.xticks(rotation=90)
-    plt.title("длины фильмов разных жанров")
+    plt.title("заболеваемость по counties")
     st.pyplot(fig)
 
     """
-    ### mpaa_rating - это степень запрещенности фильмадетям  
-
-привожу     расшифровку названий  степеней запрета: G – General Audiences
-All ages admitted. Nothing that would offend parents for viewing by children.
-PG – Parental Guidance Suggested
-Some material may not be suitable for children. Parents urged to give "parental guidance". May contain some material parents might not like for their young children.
-PG-13 – Parents Strongly Cautioned
-Some material may be inappropriate for children under 13. Parents are urged to be cautious. Some material may be inappropriate for pre-teenagers.
-R – Restricted
-Under 17 requires accompanying parent or adult guardian. Contains some adult material. Parents are urged to learn more about the film before taking their young children with them.
-NC-17 – Adults Only.  
-рисунок 2
+    ###  рисунок 2
     """
     sns.set(rc={'axes.facecolor': 'lightblue', 'figure.facecolor': 'lightgreen'})
     fig, ax = plt.subplots()
-    sns.barplot(x="budget", y="mpaa_rating", data=df)
-    plt.title("связь бюджета с категорией запрещен детям")
+    sns.barplot(x="deaths", y="state", data=df_cov_11)
+    plt.title("смертность по counties")
     st.pyplot(fig)
 
     """
@@ -57,16 +47,16 @@ NC-17 – Adults Only.
     """
 
     fig, ax = plt.subplots()
-    sns.histplot(data=df, x='mpaa_rating', y='rating', cbar=True)
-    plt.title("успешность фильма в зависимости от категории запрещен детям")
+    sns.histplot(data=df_cov_11, x='cases', y='deaths', cbar=True)
+    plt.title("смертность в зависимости от заболеваемости")
     st.pyplot(fig)
 
     """
         ## библиотека альтаир
     """
 
-    fig1 = alt.Chart(df).mark_point().encode(x='budget', y='runtime', color=alt.Color('genre', legend=alt.Legend(title="genre by color")),
-                                        tooltip=['release_date', 'budget']).interactive()
+    fig1 = alt.Chart(df_cov_11).mark_point().encode(x='deaths', y='cases', color=alt.Color('state', legend=alt.Legend(title="state by color")),
+                                        tooltip=['county', 'deaths']).interactive()
 
     st.altair_chart(
         (
